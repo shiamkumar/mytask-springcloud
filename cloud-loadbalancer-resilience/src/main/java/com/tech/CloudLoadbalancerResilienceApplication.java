93c9c501b7d83e71cbae181650159a4b0b53e564
shiamkumar <41625556+shiamkumar@users.noreply.github.com>
2023-07-06 16:16:21 +0530

    first-commit
    first-commit

---
+++ cloud-loadbalancer-resilience/src/main/java/com/tech/CloudLoadbalancerResilienceApplication.java
@@ -0,0 +1,13 @@
+package com.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudLoadbalancerResilienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudLoadbalancerResilienceApplication.class, args);
+	}
+
+}
